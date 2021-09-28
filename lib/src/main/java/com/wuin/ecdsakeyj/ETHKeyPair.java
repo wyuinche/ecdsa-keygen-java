f610f0ac8197eccdeb2bcbd876f70d9539e445d6
wyuinche <wyuinche.mido@gmail.com>
2021-09-28 11:45:22 +0900

    Update license headers

--- lib/src/main/java/com/wuin/ecdsakeyj/ETHKeyPair.java
+++ lib/src/main/java/com/wuin/ecdsakeyj/ETHKeyPair.java
@@ -1,5 +1,4 @@
-/* Copyright 2021 BitcoinJ
- * Copyright 2021 Web3 Labs Ltd.
+/* Copyright 2021 wyuinche
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
